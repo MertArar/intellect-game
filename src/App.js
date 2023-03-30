e417da178c5c04015320c21339af8dc5ad7b7045
MertArar <mertarar.ma@gmail.com>
2023-03-30 03:15:28 +0300

    change in architecture

--- src/App.js
+++ src/App.js
@@ -1,80 +1,13 @@
+import React from "react";
 import "./App.css";
-import Images from "./images";
-import { useState } from "react";
-import { shuffle } from "lodash";
+import Game from "./components/Game/Game";
 
-function App() {
-  const [cards, setCards] = useState(shuffle([...Images, ...Images]));
-  const [clicks, setClicks] = useState(0);
-  const [won, setWon] = useState(false);
-  const [activeCards, setActiveCards] = useState([]);
-  const [foundPairs, setFoundPairs] = useState([]);
-
-  function flipCard(index) {
-    if (won) {
-      setCards(shuffle([...Images, ...Images]));
-      setFoundPairs([]);
-      setWon(false);
-      setClicks(0);
-    }
-    if (activeCards.length === 0) {
-      setActiveCards([index]);
-    }
-    if (activeCards.length === 1) {
-      const firstIndex = activeCards[0];
-      const secondsIndex = index;
-      if (cards[firstIndex] === cards[secondsIndex]) {
-        if (foundPairs.length + 2 === cards.length) {
-          setWon(true);
-        }
-        setFoundPairs([...foundPairs, firstIndex, secondsIndex]);
-      }
-      setActiveCards([...activeCards, index]);
-    }
-    if (activeCards.length === 2) {
-      setActiveCards([index]);
-    }
-    setClicks(clicks + 1);
-  }
-
+const App = () => {
   return (
     <div>
-      <div className="board">
-        {cards.map((card, index) => {
-          const flippedToFront =
-            activeCards.indexOf(index) !== -1 ||
-            foundPairs.indexOf(index) !== -1;
-          return (
-            <div
-              className={"card-outer " + (flippedToFront ? "flipped" : "")}
-              onClick={() => flipCard(index)}
-            >
-              <div className="card">
-                <div className="front">
-                  <img src={card} alt="" />
-                </div>
-                <div className="back" />
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="stats">
-        {won && (
-          <>
-            Tebrikler, kazandınız!
-            <br />
-            Tekrar oynamak için herhangi bir karta tıklayın.
-            <br />
-            <br />
-          </>
-        )}
-        Tıklama Sayısı: {clicks} &nbsp;&nbsp;&nbsp; Eşleşme:{" "}
-        {foundPairs.length / 2}
-      </div>
-      <div className="sign">Powered by Mert ARAR</div>
+      <Game />
     </div>
   );
-}
+};
 
 export default App;
